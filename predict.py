58c4713657acedd01fa1e0ccc48e48bc45ba1ee9
wow-amazing <nb2000@yeah.net>
2020-12-18 00:30:13 +0800

    增加预测部分

---
+++ predict.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/12/17 23:24
+# @Author : gsg
+# @Site : 
+# @File : predict.py
+# @Software: PyCharm
+import gc
+import os
+import pandas as pd
+
+import torch
+from scipy.sparse import load_npz
+from sklearn.preprocessing import LabelEncoder
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+feature_path = './feature/'
+model_path = './model/'
+
+model = torch.load(os.path.join(model_path, 'checkpoint'))
+matrix = pd.read_csv(os.path.join(feature_path, 'data.csv'))
+
+# train_data = matrix[matrix['origin'] == 'train'].drop(['origin'], axis=1)
+test_data = matrix[matrix['origin'] == 'test'].drop(['label', 'origin'], axis=1)
+# print(np.array(train_data).shape)
+# print(np.array(test_data).shape)
+# train_X, train_y = train_data.drop(['label'], axis=1), train_data['label']
+del matrix
+gc.collect()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class DDataset(Dataset):
+    def __init__(self):
+        x = np.array(test_data)
+        # print(x.shape)
+        self.x_data = torch.from_numpy(x)
+        self.len = self.x_data.shape[0]
+        # print(self.len)
+
+    def __getitem__(self, item):
+        return self.x_data[item]
+
+    def __len__(self):
+        return self.len
+
+
+test_set = DDataset()
+test_loader = DataLoader(dataset=test_set, batch_size=128, shuffle=False)
+preds = []
+model.eval()
+
+with torch.no_grad():
+    for feature in test_loader:
+        feature = feature.float().to(device)
+        pred = model(feature)
+        preds += pred.cpu().data
+
+mmp = []
+for i in preds:
+    fuck = []
+    for ii in i:
+        fuck.append(ii.item())
+    mmp.append(fuck)
+
+print(mmp)
+
+pred = pd.DataFrame(mmp, index=test_data.index)
+
+pred.to_csv('pred.csv', index=True)
